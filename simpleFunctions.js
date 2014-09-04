715400c3cbdd385c7bbde5e0b5b9a47b199ec4e5
stephen-standridge <stephen.standridge@omadahealth.com>
2014-09-04 09:41:55 -0700

    reverted

---
+++ simpleFunctions.js
@@ -0,0 +1,14 @@
+define(function () {
+  arrayCheck = function(needle, haystack){
+    var length = haystack.length;
+    for(var i = 0; i < length; i++) {
+        if(haystack[i] == needle) return true;
+    }
+    return false;
+  }
+  var simpleFunctions = {
+    inArray : arrayCheck,
+  }
+
+  return simpleFunctions
+})
